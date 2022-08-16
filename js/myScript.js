2b159d29d5dc2bbbeeabc2dfa1a1aa6d927ae3e8
OlegJak <100533386+OlegJak@users.noreply.github.com>
2022-08-16 12:53:49 +0300

    Add files via upload

--- js/myScript.js
+++ js/myScript.js
@@ -113,4 +113,10 @@ $(".eng").click(function () {
 });
 
 
-var hellopreloader = document.getElementById("hellopreloader_preload");function fadeOutnojquery(el){el.style.opacity = 1;var interhellopreloader = setInterval(function(){el.style.opacity = el.style.opacity - 0.05;if (el.style.opacity <=0.05){ clearInterval(interhellopreloader);hellopreloader.style.display = "none";}},16);}window.onload = function(){setTimeout(function(){fadeOutnojquery(hellopreloader);},1000);};
+$(window).on('load', function () {
+    $('body').addClass('loaded_hiding');
+        window.setTimeout(function () {
+            $('body').addClass('loaded');
+            $('body').removeClass('loaded_hiding');
+        }, 500);
+});
